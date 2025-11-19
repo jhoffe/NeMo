f337bdec88603047822e8683ccd4a33276fd7a54
Jonas Hoffmann <jhoffe.gh@proton.me>
2025-11-19 10:56:27 +0100

    Add print statement to display frequency and time mask parameters in SpectrogramAugmentation and SpecAugment classes

--- nemo/collections/asr/modules/audio_preprocessing.py
+++ nemo/collections/asr/modules/audio_preprocessing.py
@@ -824,6 +824,10 @@ class SpectrogramAugmentation(NeuralModule):
     ):
         super().__init__()
 
+        print(
+            f"freq_masks: {self.freq_masks}, time_masks: {self.time_masks}, freq_width: {self.freq_width}, time_width: {self.time_width}"
+        )
+
         if rect_masks > 0:
             self.spec_cutout = SpecCutout(
                 rect_masks=rect_masks,
